6da99d325fb69467f131babc0e06c2e077f22b51
Alaina Sliwinski <asliwinski23@gmail.com>
2021-05-13 19:05:30 -0400

    accidentally wrote line data to wrong file, needed to update

--- Mapping_Multiple_Points/static/static/js/logic.js
+++ Mapping_Multiple_Points/static/static/js/logic.js
@@ -6,27 +6,14 @@ console.log("working");
 // The mapid will reference the id tag in our <div> element on the index.html file.
 // The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18.
 // An alternative to using the setView() method is to modify each attribute in the map object using the curly braces notation as follows:
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-// Coordinates for each point to be used in the line.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+let map = L.map('mapid').setView([39.8283, -98.5795], 4);
 
 // We create the tile layer that will be the background of our map.
 // USING LEAFLET DOCUMENTATION
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -37,3 +24,14 @@ streets.addTo(map);
 // Get data from cities.js
 let cityData = cities;
 
+  // Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location,{
+        radius: city.population/100000,
+        color: "pink",
+        fillColor: "orange"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+   });
